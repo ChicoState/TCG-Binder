821b5c5ad510bb7a09e16267925bc42c78f9e281
xutorres <xtorres3@csuchico.edu>
2024-03-03 18:01:03 -0800

    created splash page and menu buttons

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from homepage import views
 
 urlpatterns = [
-        path("", views.index, name="index"),
+        path('', views.splash, name='splash'),
 ]
